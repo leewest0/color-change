834db16bd068bbbfb35ab423f1001527b40fd9b4
leewest0 <fusionmedia17@gmail.com>
2020-06-01 14:03:07 +0000

    first commit

---
+++ js/inch.js
@@ -0,0 +1,34 @@
+
+  var grey = "rgb(51, 51, 51)";  
+  var tomato = "#d40640";
+  var blue = "rgb(22, 65, 204)";
+  var green = "rgb(16, 192, 60)";
+  var reset = "#eee";
+
+function functionGrey()
+ {
+  
+  document.body.style.backgroundColor = grey;
+}
+
+
+function functionTomato() {
+    document.body.style.backgroundColor = tomato;
+  }
+  
+
+function functionBlue() {
+  document.body.style.backgroundColor = blue;
+}
+
+
+function functionGreen() {
+  document.body.style.backgroundColor = green;
+}
+
+  
+function functionReset() {
+    document.body.style.backgroundColor = reset;
+  }
+  
+    
